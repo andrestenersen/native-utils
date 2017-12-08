53a738187bfdf74e7dbbc20fbed3408086c3e58c
andr3 <andre.stenersen@gmail.com>
2017-12-09 00:50:50 +0100

    added loadLibraryFromJarAndReturnPath function

--- src/main/java/cz/adamh/utils/NativeUtils.java
+++ src/main/java/cz/adamh/utils/NativeUtils.java
@@ -23,24 +23,22 @@
  */
 package cz.adamh.utils;
 
-import java.io.*;
-import java.nio.file.FileSystemNotFoundException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.ProviderNotFoundException;
-import java.nio.file.StandardCopyOption;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.*;
 
 /**
  * A simple library class which helps with loading dynamic libraries stored in the
  * JAR archive. These libraries usualy contain implementation of some methods in
  * native code (using JNI - Java Native Interface).
- * 
- * @see http://adamheinrich.com/blog/2012/how-to-load-native-jni-library-from-jar
- * @see https://github.com/adamheinrich/native-utils
  *
+ * @see <a href="http://adamheinrich.com/blog/2012/how-to-load-native-jni-library-from-jar"/>
+ * @see <a href="https://github.com/adamheinrich/native-utils"/>
  */
 public class NativeUtils {
- 
+
     /**
      * The minimum length a prefix for a file has to have according to {@link File#createTempFile(String, String)}}.
      */
@@ -59,36 +57,56 @@ public class NativeUtils {
 
     /**
      * Loads library from current JAR archive
-     * 
+     * <p>
      * The file from JAR is copied into system temporary directory and then loaded. The temporary file is deleted after
      * exiting.
      * Method uses String as filename because the pathname is "abstract", not system-dependent.
-     * 
+     *
      * @param path The path of file inside JAR as absolute path (beginning with '/'), e.g. /package/File.ext
-     * @throws IOException If temporary file creation or read/write operation fails
+     * @throws IOException              If temporary file creation or read/write operation fails
      * @throws IllegalArgumentException If source file (param path) does not exist
      * @throws IllegalArgumentException If the path is not absolute or if the filename is shorter than three characters
-     * (restriction of {@link File#createTempFile(java.lang.String, java.lang.String)}).
-     * @throws FileNotFoundException If the file could not be found inside the JAR.
+     *                                  (restriction of {@link File#createTempFile(java.lang.String, java.lang.String)}).
+     * @throws FileNotFoundException    If the file could not be found inside the JAR.
      */
     public static void loadLibraryFromJar(String path) throws IOException {
- 
+        loadLibraryFromJarAndReturnPath(path);
+    }
+
+    /**
+     * Loads library from current JAR archive and returns the path to newly loaded file
+     * <p>
+     * The file from JAR is copied into system temporary directory and then loaded. The temporary file is deleted after
+     * exiting.
+     * Method uses String as filename because the pathname is "abstract", not system-dependent.
+     *
+     * @param path The path of file inside JAR as absolute path (beginning with '/'), e.g. /package/File.ext
+     * @throws IOException              If temporary file creation or read/write operation fails
+     * @throws IllegalArgumentException If source file (param path) does not exist
+     * @throws IllegalArgumentException If the path is not absolute or if the filename is shorter than three characters
+     *                                  (restriction of {@link File#createTempFile(java.lang.String, java.lang.String)}).
+     * @throws FileNotFoundException    If the file could not be found inside the JAR.
+     */
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public static String loadLibraryFromJarAndReturnPath(String path) throws IOException {
+
+
         if (!path.startsWith("/")) {
             throw new IllegalArgumentException("The path has to be absolute (start with '/').");
         }
- 
+
         // Obtain filename from path
         String[] parts = path.split("/");
         String filename = (parts.length > 1) ? parts[parts.length - 1] : null;
- 
+
         // Check if the filename is okay
         if (filename == null || filename.length() < MIN_PREFIX_LENGTH) {
             throw new IllegalArgumentException("The filename has to be at least 3 characters long.");
         }
- 
+
         // Prepare temporary file
         if (temporaryDir == null) {
-            temporaryDir = createTempDirectory("nativeutils");
+            temporaryDir = createTempDirectory();
             temporaryDir.deleteOnExit();
         }
 
@@ -106,6 +124,7 @@ public class NativeUtils {
 
         try {
             System.load(temp.getAbsolutePath());
+            return temp.getAbsolutePath();
         } finally {
             if (isPosixCompliant()) {
                 // Assume POSIX compliant file system, can be deleted after loading
@@ -115,31 +134,28 @@ public class NativeUtils {
                 temp.deleteOnExit();
             }
         }
+
     }
 
     private static boolean isPosixCompliant() {
         try {
-            if (FileSystems.getDefault()
+            return FileSystems.getDefault()
                     .supportedFileAttributeViews()
-                    .contains("posix")) {
-                return true;
-            }
-            return false;
+                    .contains("posix");
         } catch (FileSystemNotFoundException
                 | ProviderNotFoundException
                 | SecurityException e) {
             return false;
         }
     }
-    
-    private static File createTempDirectory(String prefix) throws IOException
-    {
+
+    private static File createTempDirectory() throws IOException {
         String tempDir = System.getProperty("java.io.tmpdir");
-        File generatedDir = new File(tempDir, prefix + System.nanoTime());
-        
+        File generatedDir = new File(tempDir, "nativeutils" + System.nanoTime());
+
         if (!generatedDir.mkdir())
             throw new IOException("Failed to create temp directory " + generatedDir.getName());
-        
+
         return generatedDir;
     }
 }
